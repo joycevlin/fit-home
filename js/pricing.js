eaaa488a4a09c6cbad9070521763fdcfe8f3b8b3
joycevlin <78488636+joycevlin@users.noreply.github.com>
2021-04-07 23:45:03 +0700

    Add files via upload

---
+++ js/pricing.js
@@ -0,0 +1,14 @@
+var pricing = [{
+    name: 'Perform Pack',
+    details: '$70',
+    picture: 'https://images.unsplash.com/photo-1594381898411-846e7d193883?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80'
+}, {
+    name: 'Serenity Pack',
+    details: '$60',
+    picture: 'https://images.unsplash.com/photo-1547852356-b20668106c51?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=668&q=80'
+}, {
+    name: 'Foundation Pack',
+    details: '$65',
+    picture: 'https://images.unsplash.com/photo-1617133973515-f714c8e8f51e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80'
+    
+}]
